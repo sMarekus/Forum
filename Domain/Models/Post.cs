c3d011a428ecbce17688979c8f44c88cb13563f4
Marekus <marek.stransky1@gmail.com>
2023-10-22 18:24:18 +0200

    Models: Post and User classes

---
+++ Domain/Models/Post.cs
@@ -0,0 +1,16 @@
+﻿namespace Domain.Models;
+
+public class Post
+{
+    public int id { get; set; }
+    public User owner { get; }
+    public string title { get; }
+    public string description { get; }
+
+    public Post(User owner, string title, string description)
+    {
+        this.owner = owner;
+        this.title = title;
+        this.description = description;
+    }
+}
