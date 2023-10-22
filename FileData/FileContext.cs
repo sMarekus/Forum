c3e611db2deabd1c38f065d99389fd31560560c0
mtezxx <118538867+mtezxx@users.noreply.github.com>
2023-10-22 18:41:38 +0200

    FileContext: functionality

--- FileData/FileContext.cs
+++ FileData/FileContext.cs
@@ -1,6 +1,52 @@
-﻿namespace FileData;
+﻿using System.Text.Json;
+using Domain.Models;
 
+namespace FileData;
 public class FileContext
 {
-    
+    private const string filePath = "data.json";
+    private DataContainer? dataContainer;
+
+    public ICollection<Post> Posts
+    {
+        get
+        {
+            LoadData();
+            return dataContainer!.Posts;
+        }   
+    }
+
+    public ICollection<User> Users
+    {
+        get
+        {
+            LoadData();
+            return dataContainer!.Users;
+        }
+    }
+
+    private void LoadData()
+    {
+        if (dataContainer != null) return;
+
+        if (!File.Exists(filePath))
+        {
+            dataContainer = new()
+            {
+                Posts = new List<Post>(),
+                Users = new List<User>()
+            };
+            return;
+        }
+
+        string content = File.ReadAllText(filePath);
+        dataContainer = JsonSerializer.Deserialize<DataContainer>(content);
+    }
+
+    public void SaveChanges()
+    {
+        string serialized = JsonSerializer.Serialize(dataContainer);
+        File.WriteAllText(filePath,serialized);
+        dataContainer = null;
+    }
 }
