31d82d620da1ae7383e752c70dc5e44d3da66f30
mtezxx <118538867+mtezxx@users.noreply.github.com>
2023-10-23 17:21:18 +0200

    RegisterIser: Blazor

---
+++ BlazorWASM/Program.cs
@@ -0,0 +1,20 @@
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using BlazorWASM;
+using HttpClients.ClientInterfaces;
+using HttpClients.Implementations;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped<IUserService, UserHttpClient>();
+
+builder.Services.AddScoped(
+    sp => 
+        new HttpClient { 
+            BaseAddress = new Uri("https://localhost:7175") 
+        }
+);
+
+await builder.Build().RunAsync();
