65493070ca33617a9535d8cefbeabda5b3259610
mtezxx <118538867+mtezxx@users.noreply.github.com>
2023-10-23 16:47:58 +0200

    CreateUser: Implementation

---
+++ Application/DaoInterfaces/IUserDao.cs
@@ -0,0 +1,9 @@
+﻿using Domain.Models;
+
+namespace Application.DaoInterfaces;
+
+public interface IUserDao
+{
+    Task<User> CreateAsync(User user);
+    Task<User?> GetByUsernameAsync(string userName);
+}
---
+++ Application/Logic/UserLogic.cs
@@ -0,0 +1,60 @@
+﻿using Application.DaoInterfaces;
+using Application.LogicInterfaces;
+using Domain.DTOs;
+using Domain.Models;
+
+namespace Application.Logic;
+
+public class UserLogic : IUserLogic
+{
+    private readonly IUserDao userDao;
+
+    public UserLogic(IUserDao userDao)
+    {
+        this.userDao = userDao;
+    }
+
+    public async Task<User> CreateAsync(UserCreationDto dto)
+    {
+        User? existing = await userDao.GetByUsernameAsync(dto.UserName);
+        if (existing != null)
+        {
+            throw new Exception("Username is already taken! ");
+        }
+
+        ValidateData(dto);
+        User toCreate = new User
+        {
+            Username = dto.UserName,
+            Password = dto.Password
+        };
+
+        User created = await userDao.CreateAsync(toCreate);
+        
+        return created;
+    }
+
+    private static void ValidateData(UserCreationDto userToCreate)
+    {
+        string userName = userToCreate.UserName;
+        string password = userToCreate.Password;
+
+        if (userName.Length <= 1)
+        {
+            throw new Exception("Username must contain at least 1 character! ");
+        }
+        else if (userName.Length >= 15)
+        {
+            throw new Exception("Username must be less then 15 characters! ");
+        }
+
+        if (password.Length <= 3)
+        {
+            throw new Exception("Password must contain at least 3 character! ");
+        } 
+        else if (password.Length >= 15)
+        {
+            throw new Exception("Password must be less then 15 characters! ");
+        }
+    }
+}
